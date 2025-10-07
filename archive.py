72ef9246abb456f542b0d85f34d502421d8ac7d9
Yann Zerlaut <yann.zerlaut@gmail.com>
2025-10-07 08:20:14 +0200

    rm previous archive

--- archive.py
+++ archive.py
@@ -1,22 +1,38 @@
-import datetime
-import sys, os, shutil
+import datetime, pathlib
+import sys, os
 import git # pip install gitpython
+
+ARCHIVE = '_archives'
 
 repo = git.Repo(os.path.dirname(__file__))
 
-if os.path.isfile(sys.argv[-1]):
+# if os.path.isfile(sys.argv[-1]):
+if True:
+
     timestamp = \
         datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
-    print(sys.argv[-1])
+
+    pathlib.Path.mkdir(os.path.join(ARCHIVE,
+                    datetime.datetime.now().strftime("%Y")))
+
+    folder = os.path.join(ARCHIVE,
+                          datetime.datetime.now().strftime("%Y"),
+                          datetime.datetime.now().strftime("%B"))
+
+    pathlib.Path.mkdir(folder, exist_ok=True)
+    
     filename = '%s-%s' %(timestamp,
                          os.path.basename(sys.argv[-1]))
+
     shutil.copy(sys.argv[-1], 
-                os.path.join('_archives', filename))
+                os.path.join(folder, filename))
+
     print("""
         archiving '%s' as:
             %s 
           """ % (sys.argv[-1],
-                 os.path.join('_archives', filename)))
-    repo.index.add(os.path.join('_archives', filename))
+                 os.path.join(folder, filename)))
+
+    repo.index.add(os.path.join(folder, filename))
     repo.index.commit('add '+filename) 
     repo.remotes.origin.push()
