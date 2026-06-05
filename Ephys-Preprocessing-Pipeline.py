242bb499fbb35d3b870711da792abd6a5fec0a69
Yann Zerlaut <yann.zerlaut@gmail.com>
2026-06-05 18:04:25 +0200

    up

--- Ephys-Preprocessing-Pipeline.py
+++ Ephys-Preprocessing-Pipeline.py
@@ -1,16 +1,21 @@
 # %%
 import spikeinterface.sorters as ss
 import spikeinterface.full as si
+
 rec = si.read_openephys(\
     os.path.join(\
         os.path.expanduser('~'),
             'DATA/2026_04_24/2026-04-24_12-23-16'),
                 stream_name='Record Node 101#OneBox-100.ProbeA')
 
+rec = rec.select_channels(rec.get_channel_ids()[::8])
+rec = rec.frame_slice(1000000, 1300000) # 300k frames
+rec = rec.save(folder='/tmp')
+
 # %%
-ss.get_default_sorter_params('kilosort4')
 # %%
-ss.installed_sorters()
-# %%
-ss.install_package_in_container?
-# %%
+sorting = ss.run_sorter(sorter_name='kilosort4', 
+                        recording=rec,
+                        folder="/tmp/kilosort3", 
+                        docker_image=True)
+
