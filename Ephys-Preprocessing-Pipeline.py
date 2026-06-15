fef332e57c4ecf41f7fcbd46157da77ad53aab95
Yann Zerlaut <yann.zerlaut@gmail.com>
2026-06-15 17:15:06 +0200

    up

--- Ephys-Preprocessing-Pipeline.py
+++ Ephys-Preprocessing-Pipeline.py
@@ -3,6 +3,12 @@ import os
 import spikeinterface.sorters as ss
 import spikeinterface.full as si
 
+##
+# sudo -i # to turn root ()
+# /home/user/miniforge3/bin/python /home/user/lab-notebook/sally/Ephys-Preprocessing-Pipeline.py
+
+
+
 # 1) finish to run the transfer:
 # rsync -avhP --exclude "*.bin" ~/DATA/Sally/2026_04_24 user@10.0.0.1:DATA/Sally
 
@@ -18,19 +24,18 @@ rec = si.read_openephys(\
 rec = rec.select_channels(rec.get_channel_ids()[::8])
 
 # here just for speed
-rec = rec.frame_slice(1000000, 1300000) # 300k frames
-
+rec = rec.frame_slice(95455267, 293009263) # 300k frames
 
 # 4) here add the compression if possible !
 #   ideally we would only store this...
 rec = rec.save(folder='/tmp/test')
+# if already existing, just do: "rm -rf /tmp/test"
 
 # %%
 # 5) run the spike sorting through docker
 # https://spikeinterface.readthedocs.io/en/stable/modules/sorters.html#running-sorters-in-docker-singularity-containers 
 sorting = ss.run_sorter(sorter_name='kilosort4', 
                         recording=rec,
-                        # folder="/tmp/", 
                         docker_image=True)
 
 # spike interface quickstart:
