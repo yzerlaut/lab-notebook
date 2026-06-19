338dd567527b1a9913d61aaa34df7f54db457f70
Yann Zerlaut <yann.zerlaut@gmail.com>
2026-06-19 11:43:25 +0200

    rm-pywt-dep

--- Ephys-Preprocessing-Pipeline.py
+++ Ephys-Preprocessing-Pipeline.py
@@ -3,6 +3,30 @@ import os
 import spikeinterface.sorters as ss
 import spikeinterface.full as si
 
+# %%
+import os, sys, json
+import numpy as np
+import pandas as pd
+
+sys.path += ['physion/src']
+from physion.assembling.add_ephys import read_kilosort
+kilosort_folder = os.path.join(
+            os.path.expanduser('~/DATA/Sally/2026_06_09/2026-06-09_17-24-45'),
+            'Record Node 101', 'experiment1', 'recording1', 
+            'continuous', 'OneBox-100.ProbeA', 'kilosort4')
+sorting = read_kilosort(kilosort_folder)
+templates = sorting['templates']
+cInfo = pd.read_csv(\
+    open(os.path.join(kilosort_folder,
+                'cluster_info.tsv')), sep = '\t')
+
+# %%
+from physion.utils import plot_tools as pt 
+pt.set_style('dark')
+from spikeinterface import full as si
+
+# %%
+
 ##
 # sudo -i # to turn root ()
 # /home/user/miniforge3/bin/python /home/user/lab-notebook/sally/Ephys-Preprocessing-Pipeline.py
@@ -14,17 +38,17 @@ import spikeinterface.full as si
 
 rec = si.read_openephys(\
     os.path.join(\
-        # os.path.expanduser('~'),
-            '/home/user/DATA/Sally/2026_04_24/2026-04-24_12-23-16'),
+            os.path.expanduser('~/DATA/Sally/2026_06_09/2026-06-09_17-24-45')),
                 stream_name='Record Node 101#OneBox-100.ProbeA')
 
+# %%
 # 2) subselect channels nicely here 
 #   - remove bad channels
 #   - focus on interesting part of the probe
 rec = rec.select_channels(rec.get_channel_ids()[::8])
 
 # here just for speed
-rec = rec.frame_slice(95455267, 293009263) # 300k frames
+# rec = rec.frame_slice(95455267, 293009263) # 300k frames
 
 # 4) here add the compression if possible !
 #   ideally we would only store this...
@@ -40,3 +64,27 @@ sorting = ss.run_sorter(sorter_name='kilosort4',
 
 # spike interface quickstart:
 # https://spikeinterface.readthedocs.io/en/stable/get_started/quickstart.html
+
+# %%
+cInfo
+# %%
+def find_matching_unit(id):
+    cond = (sorting['spike_clusters']==id)
+    return np.unique(sorting['spike_templates'][cond])[0]
+
+find_matching_unit(594)
+# %%
+# print(sorting['spike_templates'])
+np.unique(sorting['spike_templates'])
+
+# %%
+np.unique(sorting['spike_detection_templates']).shape
+# print(sorting['spike_detection_templates'].shape)
+
+# %%
+np.unique(sorting['spike_clusters'])
+
+# %%
+import spikeinterface.full as si
+sorting = si.read_kilosort(kilosort_folder)
+# %%
