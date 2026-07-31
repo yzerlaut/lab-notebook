d468b971bf61fcffc6791080b6618e4b6795b257
yann <yann.zerlautr@proton.me>
2026-07-31 15:40:04 +0200

    update-ephys-spike-sorting

--- Ephys-Preprocessing-Pipeline.py
+++ Ephys-Preprocessing-Pipeline.py
@@ -2,30 +2,8 @@
 import os
 import spikeinterface.sorters as ss
 import spikeinterface.full as si
-
-# %%
-import os, sys, json
-import numpy as np
-import pandas as pd
-
-sys.path += ['physion/src']
-from physion.assembling.add_ephys import read_kilosort
-kilosort_folder = os.path.join(
-            os.path.expanduser('~/DATA/Sally/2026_06_09/2026-06-09_17-24-45'),
-            'Record Node 101', 'experiment1', 'recording1', 
-            'continuous', 'OneBox-100.ProbeA', 'kilosort4')
-sorting = read_kilosort(kilosort_folder)
-templates = sorting['templates']
-cInfo = pd.read_csv(\
-    open(os.path.join(kilosort_folder,
-                'cluster_info.tsv')), sep = '\t')
-
-# %%
-from physion.utils import plot_tools as pt 
-pt.set_style('dark')
-from spikeinterface import full as si
-
-# %%
+import spikeinterface.exporters as sexp
+import spikeinterface.widgets as sw
 
 ##
 # sudo -i # to turn root ()
@@ -38,21 +16,23 @@ from spikeinterface import full as si
 
 rec = si.read_openephys(\
     os.path.join(\
-            os.path.expanduser('~/DATA/Sally/2026_06_09/2026-06-09_17-24-45')),
+        # os.path.expanduser('~'),
+            '/media/user/DATA2/2026-06-09_17-24-45'),
                 stream_name='Record Node 101#OneBox-100.ProbeA')
 
-# %%
 # 2) subselect channels nicely here 
 #   - remove bad channels
 #   - focus on interesting part of the probe
 rec = rec.select_channels(rec.get_channel_ids()[::8])
 
 # here just for speed
-# rec = rec.frame_slice(95455267, 293009263) # 300k frames
+#rec = rec.frame_slice(100000, 1100000) # 10M frames
 
 # 4) here add the compression if possible !
 #   ideally we would only store this...
-rec = rec.save(folder='/tmp/test')
+rec = rec.save(folder='/tmp/test',
+                   overwrite=True)
+
 # if already existing, just do: "rm -rf /tmp/test"
 
 # %%
@@ -60,31 +40,43 @@ rec = rec.save(folder='/tmp/test')
 # https://spikeinterface.readthedocs.io/en/stable/modules/sorters.html#running-sorters-in-docker-singularity-containers 
 sorting = ss.run_sorter(sorter_name='kilosort4', 
                         recording=rec,
+                        verbose=True,
                         docker_image=True)
+
+analyzer = si.create_sorting_analyzer(\
+        sorting=sorting,
+        recording=rec,
+        format='binary_folder',
+        overwrite=True,
+        folder='analyzer_binary')
+        
+extensions_to_compute = [
+    "random_spikes",
+    "waveforms",
+    "noise_levels",
+    "templates",
+    "spike_amplitudes",
+    "unit_locations",
+    "spike_locations",
+    "correlograms",
+    "template_similarity"
+]
+
+extension_params = {
+    "unit_locations": {"method": "center_of_mass"},
+    "spike_locations": {"ms_before": 0.1},
+    "correlograms": {"bin_ms": 0.1},
+    "template_similarity": {"method": "cosine_similarity"}
+}
+
+analyzer.compute(extensions_to_compute,
+                 extension_params=extension_params)
+
+if os.path.isdir("./phy_folder"):
+    os.rmdir("./phy_folder")
+
+sexp.export_to_phy(analyzer, "./phy_folder",
+                    verbose=True)
 
 # spike interface quickstart:
 # https://spikeinterface.readthedocs.io/en/stable/get_started/quickstart.html
-
-# %%
-cInfo
-# %%
-def find_matching_unit(id):
-    cond = (sorting['spike_clusters']==id)
-    return np.unique(sorting['spike_templates'][cond])[0]
-
-find_matching_unit(594)
-# %%
-# print(sorting['spike_templates'])
-np.unique(sorting['spike_templates'])
-
-# %%
-np.unique(sorting['spike_detection_templates']).shape
-# print(sorting['spike_detection_templates'].shape)
-
-# %%
-np.unique(sorting['spike_clusters'])
-
-# %%
-import spikeinterface.full as si
-sorting = si.read_kilosort(kilosort_folder)
-# %%
