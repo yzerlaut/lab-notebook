38e885ebd43aa53711376c2d6f2dccc915d8f0be
yann <yann.zerlautr@proton.me>
2026-08-07 13:13:30 +0200

    up

--- Ephys-Preprocessing-Pipeline.py
+++ Ephys-Preprocessing-Pipeline.py
@@ -1,5 +1,5 @@
 # %%
-import os
+import os, sys
 import spikeinterface.sorters as ss
 import spikeinterface.full as si
 import spikeinterface.exporters as sexp
@@ -14,69 +14,90 @@ import spikeinterface.widgets as sw
 # 1) finish to run the transfer:
 # rsync -avhP --exclude "*.bin" ~/DATA/Sally/2026_04_24 user@10.0.0.1:DATA/Sally
 
-rec = si.read_openephys(\
-    os.path.join(\
-        # os.path.expanduser('~'),
-            '/media/user/DATA2/2026-06-09_17-24-45'),
-                stream_name='Record Node 101#OneBox-100.ProbeA')
+for folder in os.listdir(sys.argv[-1]):
 
-# 2) subselect channels nicely here 
-#   - remove bad channels
-#   - focus on interesting part of the probe
-rec = rec.select_channels(rec.get_channel_ids()[::8])
+    print()
+    print('    running "%s" ' % folder)
+    print()
 
-# here just for speed
-#rec = rec.frame_slice(100000, 1100000) # 10M frames
+    if True:
 
-# 4) here add the compression if possible !
-#   ideally we would only store this...
-rec = rec.save(folder='/tmp/test',
-                   overwrite=True)
+        rec = si.read_openephys(\
+            os.path.join(sys.argv[-1], folder),
+                # os.path.expanduser('~'),
+                    # '/media/user/DATA2/2026-06-09_17-24-45'),
+                        stream_name='Record Node 101#OneBox-100.ProbeA')
 
-# if already existing, just do: "rm -rf /tmp/test"
+        # 2) subselect channels nicely here 
+        #   - remove bad channels
+        #   - focus on interesting part of the probe
+        # rec = rec.select_channels(rec.get_channel_ids()[::8])
+        # rec = rec.select_channels(rec.get_channel_ids()[:150])
 
-# %%
-# 5) run the spike sorting through docker
-# https://spikeinterface.readthedocs.io/en/stable/modules/sorters.html#running-sorters-in-docker-singularity-containers 
-sorting = ss.run_sorter(sorter_name='kilosort4', 
-                        recording=rec,
-                        verbose=True,
-                        docker_image=True)
+        # here just for speed
+        # rec = rec.frame_slice(100000, 1100000) # 10M frames
 
-analyzer = si.create_sorting_analyzer(\
-        sorting=sorting,
-        recording=rec,
-        format='binary_folder',
-        overwrite=True,
-        folder='analyzer_binary')
-        
-extensions_to_compute = [
-    "random_spikes",
-    "waveforms",
-    "noise_levels",
-    "templates",
-    "spike_amplitudes",
-    "unit_locations",
-    "spike_locations",
-    "correlograms",
-    "template_similarity"
-]
+        # 4) here add the compression if possible !
+        #   ideally we would only store this...
 
-extension_params = {
-    "unit_locations": {"method": "center_of_mass"},
-    "spike_locations": {"ms_before": 0.1},
-    "correlograms": {"bin_ms": 0.1},
-    "template_similarity": {"method": "cosine_similarity"}
-}
+        # rec = rec.save(folder='/media/user/DATA2/temp',
+                       # overwrite=True)
 
-analyzer.compute(extensions_to_compute,
-                 extension_params=extension_params)
+        # if already existing, just do: "rm -rf /tmp/test"
 
-if os.path.isdir("./phy_folder"):
-    os.rmdir("./phy_folder")
+        # %%
+        # 5) run the spike sorting through docker
+        # https://spikeinterface.readthedocs.io/en/stable/modules/sorters.html#running-sorters-in-docker-singularity-containers 
 
-sexp.export_to_phy(analyzer, "./phy_folder",
-                    verbose=True)
 
-# spike interface quickstart:
-# https://spikeinterface.readthedocs.io/en/stable/get_started/quickstart.html
+        # sorting = ss.run_sorter(sorter_name='spykingcircus2', 
+        sorting = ss.run_sorter(sorter_name='kilosort4', 
+                                recording=rec,
+                                verbose=True,
+                                folder=os.path.join(sys.argv[-1], 
+                                                    'kilosort4_output'))
+
+        # sorting = ss.run_sorter(sorter_name='kilosort4', 
+                                # recording=rec,
+                                # verbose=True,
+                                # docker_image=True)
+
+        # analyzer = si.create_sorting_analyzer(\
+                # sorting=sorting,
+                # recording=rec,
+                # format='binary_folder',
+                # overwrite=True,
+                # folder=os.path.join(sys.argv[-1], 
+                                    # folder, 
+                                    # 'analyzer'))
+
+        # extensions_to_compute = [
+            # "random_spikes",
+            # "waveforms",
+            # "noise_levels",
+            # "templates",
+            # "spike_amplitudes",
+            # "unit_locations",
+            # "spike_locations",
+            # "correlograms",
+            # "template_similarity"
+        # ]
+
+        # extension_params = {
+            # "unit_locations": {"method": "center_of_mass"},
+            # "spike_locations": {"ms_before": 0.1},
+            # "correlograms": {"bin_ms": 0.1},
+            # "template_similarity": {"method": "cosine_similarity"}
+        # }
+
+        # analyzer.compute(extensions_to_compute,
+                         # extension_params=extension_params)
+
+        # if os.path.isdir("./phy_folder"):
+            # os.rmdir("./phy_folder")
+
+        # sexp.export_to_phy(analyzer, "./phy_folder",
+                            # verbose=True)
+
+        # spike interface quickstart:
+        # https://spikeinterface.readthedocs.io/en/stable/get_started/quickstart.html
